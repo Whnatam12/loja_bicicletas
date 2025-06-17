4f54cbd0432bd7630ee6a5723f551d26a90e8dae
NATAM SANTOS <59802325+Whnatam12@users.noreply.github.com>
2025-06-17 14:57:47 -0300

    Update ProdutsItem.jsx

--- LOJA_BICICLETAS/minha_loja/src/components/ProdutsItem.jsx
+++ LOJA_BICICLETAS/minha_loja/src/components/ProdutsItem.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function ProdutsItem({ produts }) {
 
-  
+function ProdutsItem({ produts ,} ) {
 
+ 
   return (
 
     <div className='produts-item' >
@@ -17,10 +17,12 @@ function ProdutsItem({ produts }) {
 
       <h3>{produts.name}</h3>
       <p>Preço: R${produts.price.toFixed(2)}</p>
-      <Link to={`/produts/${produts.id}`}>   <img src={produts.image} alt={produts.name}  /></Link>
+     
 
-   
 
+<Link to={`/produts/${produts.id}`}>   <img src={produts.image} alt={produts.name} /> <button id='button-btn'>Ver detalhes</button></Link>
+
+    
     </div>
   );
 }
